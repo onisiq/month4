7e0773ea763b1a046d3440679774cf0594028535
onisiq <karimmalovu41@gmail.com>
2026-05-02 21:52:20 +0900

    m

--- myproject/blog/models.py
+++ myproject/blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    is_published = models.BooleanField(default=True)
+    rate = models.PositiveSmallIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
