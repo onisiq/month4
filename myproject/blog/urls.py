323f1aa28e18033a298c82f0436d73513385ca23
onisiq <karimmalovu41@gmail.com>
2026-05-15 20:34:10 +0900

    hw3

--- myproject/blog/urls.py
+++ myproject/blog/urls.py
@@ -1,9 +1,20 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('register/', views.register, name='register'),
+    path(
+        'login/',
+        auth_views.LoginView.as_view(template_name='blog/login.html'),
+        name='login',
+    ),
+    path(
+        'logout/',
+        auth_views.LogoutView.as_view(next_page='post_list'),
+        name='logout',
+    ),
     path('post/', views.post_list, name='post_list_alias'),
     path('post/create/', views.post_create, name='post_create'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -119,3 +119,6 @@ STATIC_URL = 'static/'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+LOGIN_REDIRECT_URL = 'post_list'
+LOGOUT_REDIRECT_URL = 'post_list'
